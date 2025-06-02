2a1144a7c912ed874c0281d49da77f97a14b3e48
Mohamed Sherif <138435356+Avatar2001@users.noreply.github.com>
2025-06-02 09:29:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,47 @@
-from langchain.agents import initialize_agent, Tool, AgentType
-from langchain.chat_models import ChatOpenAI
+import json
+import re
 
-llm = ChatOpenAI()
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
+from sqlalchemy import create_engine, inspect
 
-tools = [
-    Tool(name="Calculator", func=lambda x: eval(x), description="Useful for math"),
-]
+db_url = "sqlite:///testdb.sqlite"
 
-agent = initialize_agent(
-    tools=tools,
-    llm=llm,
-    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-    verbose=True,
-)
+template = """
+You are a SQL generator.  When given a schema and a user question, you MUST output only the SQL statement—nothing else.  No explanation is needed.
 
-response = agent.run("What is 45 * 67?")
-print(response)
+Schema: {schema}
+User question: {query}
+Output (SQL only):
+"""
+
+model = OllamaLLM(model="deepseek-r1:8b")
+
+def extract_schema(db_url):
+    engine = create_engine(db_url)
+    inspector = inspect(engine)
+    schema = {}
+
+    for table in inspector.get_table_names():
+        columns = inspector.get_columns(table)
+        schema[table] = [col['name'] for col in columns]
+
+    return json.dumps(schema)
+
+def to_sql_query(query, schema):
+    prompt = ChatPromptTemplate.from_template(template)
+    chain = prompt | model
+
+    return clean_text(chain.invoke({"query": query, "schema": schema}))
+
+def clean_text(text: str):
+    cleaned_text = re.sub(r"<think>.*?</think>", "", text, flags=re.DOTALL)
+    return cleaned_text.strip()
+
+schema = extract_schema(db_url)
+query = st.text_area("Describe the data you want to retrieve from the database:")
+
+if query:
+    sql = to_sql_query(query, schema)
+    st.code(sql, wrap_lines=True, language="sql")
